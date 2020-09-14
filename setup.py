6f77474d3a985d09d7192c97febe4d5cecd40aa1
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-14 19:13:06 +0900

    feat(setup): add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="Rose",
+      author="Ryu ju heon",
+      author_email="SaidBySolo@gmail.com",
+      url="https://github.com/SaidBySolo/Rose",
+      version="1.3.0",
+      packages=setuptools.find_packages(),
+      description="Heliotrope python wrapper",
+      install_requires=["aiohttp"]
+      python_requires=">=3.6"
+)
