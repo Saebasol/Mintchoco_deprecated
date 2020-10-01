eba78f201b12dec8c78a025547b76747d4ee5521
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-01 14:02:23 +0900

    chore(setup): Update 1.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",
     url="https://github.com/SaidBySolo/Rose",
-    version="1.4.0",
+    version="1.5.0",
     packages=setuptools.find_packages(),
     description="Heliotrope python wrapper",
     install_requires=["aiohttp"],
