ab47777015b8c82c8cb0292a0601854f177e083f
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-20 22:52:19 +0900

    style(setup): bump up 2.0.0 to 3.0.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-import setuptools
-from setuptools import setup
+import setuptools  # type: ignore
+from setuptools import setup  # type: ignore
 
 setup(
     name="Rose",
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",
     url="https://github.com/SaidBySolo/Rose",
-    version="2.0.0",
-    packages=setuptools.find_packages(),
+    version="3.0.0",
+    packages=setuptools.find_packages(),  # type: ignore
     description="Heliotrope python wrapper",
     install_requires=["aiohttp"],
     python_requires=">=3.6",
