75974c23d50f2697f88eb2adabaceacfdc9bba5b
Ryu JuHeon <saidbysolo@gmail.com>
2021-06-14 18:28:19 +0900

    style(mintchoco): bump up 4.0.2 to 4.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",
     url="https://github.com/SaidBySolo/Mintchoco",
-    version="4.0.2",
+    version="4.0.3",
     long_description=open("README.md", "rt", encoding="UTF8").read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),  # type: ignore
