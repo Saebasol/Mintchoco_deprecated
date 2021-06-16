b1e7e23056d8ae9411a997b121f087c500a2b648
Ryu JuHeon <saidbysolo@gmail.com>
2021-06-16 18:15:21 +0900

    fix(client): key is optional and split count method

--- mintchoco/client.py
+++ mintchoco/client.py
@@ -21,30 +21,32 @@ API_URL = BASE_URL + API_VERSION
 
 
 class Client:
-    def __init__(self, hiyobot: str):
-        self.hiyobot = hiyobot
+    def __init__(self, hiyobot: Optional[str] = None):
+        self.hiyobot = {"hiyobot": hiyobot}
 
     async def request(
         self, method: str, path: str, json: Optional[dict[str, Any]] = None
     ) -> Any:
-        headers = {"hiyobot": self.hiyobot}
         url = BASE_URL + path
         if "api" in path:
             url = API_URL + path
+
+        if not self.hiyobot["hiyobot"] and "count" in path:
+            raise RuntimeError("This endpoint required key")
+
         async with aiohttp.ClientSession() as cs:
-            async with cs.request(method, url, headers=headers, json=json) as r:
+            async with cs.request(method, url, headers=self.hiyobot, json=json) as r:
                 return await r.json()
 
     async def about(self) -> HeliotropeAbout:
         return HeliotropeAbout(**await self.request("GET", "about?json=true"))
 
-    async def count(
-        self, index: Optional[int] = None
-    ) -> BaseHeliotrope | HeliotropeCount:
-        if index:
-            return BaseHeliotrope(
-                **await self.request("POST", "/api/count", {"index": index})
-            )
+    async def post_count(self, index: int) -> BaseHeliotrope | HeliotropeCount:
+        return BaseHeliotrope(
+            **await self.request("POST", "/api/count", {"index": index})
+        )
+
+    async def get_count(self) -> HeliotropeCount:
         return HeliotropeCount(**await self.request("GET", "/api/count"))
 
     async def galleryinfo(self, index: int) -> HeliotropeGalleryInfo:
@@ -65,7 +67,5 @@ class Client:
 
     async def search(self, query: str, offset=0) -> HeliotropeSearch:
         return HeliotropeSearch(
-            **await self.request(
-                "GET", f"/api/hitomi/search?q={query}&offset={offset}"
-            )
+            **await self.request("GET", f"/api/hitomi/search?q={query}&offset={offset}")
         )
